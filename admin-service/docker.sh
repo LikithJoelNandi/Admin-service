f90e24d7693a0dcd3a74cc81123a54ed955ec170
LikithJoelNandi <49515456+LikithJoelNandi@users.noreply.github.com>
2019-05-30 10:34:13 +0530

    Update docker.sh

--- admin-service/docker.sh
+++ admin-service/docker.sh
@@ -3,4 +3,4 @@ port=8080
 docker stop admin-service
 docker rm admin-service
 docker rmi admin-service
-docker run -i -d -p 8080:8080 --expose 8080 --name admin-service -t demo/admin-service
+docker run -i -d -p 8889:8888 --name admin-service -t demo/admin-service
