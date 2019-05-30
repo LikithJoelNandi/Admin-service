76425e2e03aa07bd84009b61cb41a2aadb905f2a
LikithJoelNandi <49515456+LikithJoelNandi@users.noreply.github.com>
2019-05-30 10:43:12 +0530

    Update docker.sh

--- admin-service/docker.sh
+++ admin-service/docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-port=8080
+port=8889
 docker stop admin-service
 docker rm admin-service
 docker rmi admin-service
-docker run -i -d -p 8889:8888 --name admin-service -t demo/admin-service
+docker image build -t admin-service .
+docker run -d -p 8889:8888 --name admin-service admin-service
